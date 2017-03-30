95392ac5b76d9a9aed3313fa45f6df4e878c1dd0
Steven Robertson <steverobertson@gmail.com>
2017-03-30 21:34:28 +0100

    Create AppData folder in AddIn constructor and clear previous browser cache

--- ExcelDnaExample/AddIn.cs
+++ ExcelDnaExample/AddIn.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System.IO;
+using System.Windows;
 using System.Windows.Forms.Integration;
 using ExcelDna.Integration;
 using SteveRGB.AppHostCefSharp;
@@ -7,6 +8,23 @@ namespace SteveRGB.ExcelDnaExample
 {
     public class AddIn : IExcelAddIn
     {
+        public AddIn()
+        {
+            // Prepare AppData
+            if (!Directory.Exists(Settings.AppDataPath))
+            {
+                Directory.CreateDirectory(Settings.AppDataPath);
+            }
+            else
+            {
+                // Delete the browser cache on startup.
+                var cachePath = Path.Combine(Settings.AppDataPath, "Cache", "Cache");
+                if (!Directory.Exists(cachePath)) return;
+                foreach (var filename in Directory.GetFiles(cachePath)) File.Delete(filename);
+                Directory.Delete(cachePath);
+            }
+        }
+
         void IExcelAddIn.AutoOpen()
         { }
 
